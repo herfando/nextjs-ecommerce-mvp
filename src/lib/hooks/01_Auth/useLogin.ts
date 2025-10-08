90a9093a45a63928eb95e6871bba7d29628e162c
Herfando <herfando_91@yahoo.com>
2025-10-08 21:31:52 +0700

    create hooks useLogin.ts

---
+++ src/lib/hooks/01_Auth/useLogin.ts
@@ -0,0 +1,12 @@
+import { useMutation } from '@tanstack/react-query';
+import api from '@/lib/api/apiClient';
+import { TLoginSchema } from '@/lib/validations/auth';
+
+export function useLogin() {
+    return useMutation({
+        mutationFn: async (data: TLoginSchema) => {
+            const res = await api.post('/auth/login', data);
+            return res.data; // hasil API (token dsb)
+        },
+    });
+}
