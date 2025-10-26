25571472831a5ba4382794c02704dd5188687994
Herfando <herfando_91@yahoo.com>
2025-10-26 15:57:06 +0700

    combine navbar to be one file

--- src/app/05_home/page.tsx
+++ src/app/05_home/page.tsx
@@ -1,4 +1,4 @@
-import NavbarBeforeLogin from "@/components/container/Navbar_before_login"
+import Navbar from "@/components/container/Navbar"
 import Hero from "@/components/container/Hero"
 import FeaturedProduct from "@/components/container/Featured_Product"
 import Footer from "@/components/container/Footer"
@@ -6,7 +6,7 @@ import Footer from "@/components/container/Footer"
 export default function HomeApp() {
   return (
     <>
-    <NavbarBeforeLogin />
+    <Navbar />
     <Hero />
     <FeaturedProduct />
     <Footer/>
